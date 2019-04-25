b64335aef1de12d561086988aa82fb0ac71adc6a
Tim O'Farrell <tofarr@everyonesocial.com>
2019-04-25 14:34:05 -0600

    Engine migrations run as part of project

--- lib/web_hook_client/engine.rb
+++ lib/web_hook_client/engine.rb
@@ -1,5 +1,14 @@
 module WebHookClient
   class Engine < ::Rails::Engine
     config.generators.api_only = true
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
   end
 end
